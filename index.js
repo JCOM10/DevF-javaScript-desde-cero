15268b2759e934db470df69ef49b3b88469afabe
julio <jullioc522@gmail.com>
2025-02-06 17:21:40 -0700

    Primera Actividad

---
+++ index.js
@@ -0,0 +1,10 @@
+let num1 = parseInt(prompt("Ingresa el primer número:"));
+let num2 = parseInt(prompt("Ingresa el primer número:"));
+let num3 = parseInt(prompt("Ingresa el primer número:"));
+let saludo = ("Hola mundo");
+let pregunta = false;
+
+console.log( typeof num1 + num2 + num3);
+console.log(typeof saludo);
+console.log(typeof pregunta);
+
